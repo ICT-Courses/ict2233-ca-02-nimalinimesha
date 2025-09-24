4136e958d418abaae252ef56abca35dc4f20228e
nimalinimesha <ar112382@fhss.sjp.ac.lk>
2025-09-24 19:03:39 +0530

    added deploy script and homepage

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
